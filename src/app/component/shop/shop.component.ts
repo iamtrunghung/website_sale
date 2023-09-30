ed093b5dbee733262ede006b5e945be6d01a3b24
I am Hưng <108863959+iamtrunghung@users.noreply.github.com>
2023-09-30 16:42:54 +0700

    update search name product,price,sort

--- src/app/component/shop/shop.component.ts
+++ src/app/component/shop/shop.component.ts
@@ -8,7 +8,15 @@ import { ProductsService } from '../services/products.service';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-  products: any = []
+  products: any = [];
+  searchProducts: any = [];
+  keySearch: string = '';
+  valueRangePrice: any = [0,1000];
+  optionSelected: string = '';
+  sortProducts: any = [
+    {name: 'low to high'},
+    {name: 'high to low'}
+  ]
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -18,10 +26,32 @@ export class ShopComponent implements OnInit {
   ngOnInit(): void {
     this.getProducts();
   }
+  formatterDollar = (value: number): string => `$ ${value}`;
+  parserDollar = (value: string): string => value.replace('$ ', '');
   getProducts() {
     this.product.getProducts().subscribe((res)=>{
-      this.products = res;
+      this.products = this.searchProducts = res;
     })
+  }
+  handleSearchProduct(key: any){
+    if(key === ''){
+      this.products = this.searchProducts;
+    }else{
+      this.products = this.products.filter((product: any) =>product.title.toLowerCase().includes(key.toLowerCase()));
+    }
+  }
+  handleSearchPrice(price: any){
+    this.products = this.products.filter((product: any) => product.price > price[0] && product.price <= price[1]);
+  }
+  handleSortProduct(optionSelected: any){
+    switch (optionSelected) {
+      case 'low to high':
+        this.products = this.products.sort((a: any,b: any)=> a.price - b.price);
+        break;
+      case 'high to low':
+        this.products = this.products.sort((a: any,b: any)=> b.price - a.price);
+        break;
+    }
   }
   viewDetailProduct(idProduct: any){
     this.router.navigate([`/chi-tiet-san-pham/${idProduct}`])
